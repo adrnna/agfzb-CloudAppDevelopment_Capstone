f132216c1ce16a0440941c5c82b826dbe9f18b58
adrnna <71454952+adrnna@users.noreply.github.com>
2023-07-12 20:41:55 +0200

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'https://adporebska-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'https://adporebska-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+
 CSRF_TRUSTED_ORIGINS = ["https://*.proxy.cognitiveclass.ai", "https://*.proxy.cognitiveclass.ai/*"]
 # Application definition
 
